665b861673e620f00ac7b8b7abee600aa0074656
Vibu Roshin E V <viburoshin57@gmail.com>
2024-03-27 13:12:36 +0530

    modified:   src/components/Header.jsx 	modified:   src/index.css 	modified:   tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ["./src/*.jsx", "./src/components/*.jsx"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        title: ['"Pacifico"', "cursive"],
+      },
+    },
   },
   plugins: [],
 };
